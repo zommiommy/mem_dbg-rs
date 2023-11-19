8e1025dc2dd5f3a70ec4460125f668b2484f527e
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-11-19 22:26:57 +0100

    Fixed name

--- mem_dbg/tests/test_mem_dbg.rs
+++ mem_dbg/tests/test_mem_dbg.rs
@@ -1,13 +1,1 @@
-use mem_dbg::MemSize;
-
-#[test]
-fn test_vec_capacity() {
-    let mut v = vec![Vec::with_capacity(10)];
-    v[0].push(1);
-    v[0].push(2);
-    // We consider the capacity of the inner vector
-    assert_eq!(
-        8 * std::mem::size_of::<i32>(),
-        v.mem_capacity() - v.mem_size()
-    );
-}
+use mem_dbg::MemDbg;
