3eebda1fcbc4bb81094e15b4f1ee56ed314ebba3
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-08-09 10:29:44 +0200

    Formatted code

--- mem_dbg/tests/test_mem_size_no_import.rs
+++ mem_dbg/tests/test_mem_size_no_import.rs
@@ -6,6 +6,9 @@ struct MyTestStruct(i32);
 #[test]
 fn test_mem_size_no_import() {
     let my_test_struct = MyTestStruct(42);
-    let mem_size = <MyTestStruct as mem_dbg::MemSize>::mem_size(&my_test_struct, mem_dbg::SizeFlags::default());
+    let mem_size = <MyTestStruct as mem_dbg::MemSize>::mem_size(
+        &my_test_struct,
+        mem_dbg::SizeFlags::default(),
+    );
     assert_eq!(mem_size, 4);
 }
