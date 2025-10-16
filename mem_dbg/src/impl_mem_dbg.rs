fed52fba394a051befd4f7fdb83d155c4a67a0c7
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-10-16 12:28:00 +0200

    fmt

--- mem_dbg/src/impl_mem_dbg.rs
+++ mem_dbg/src/impl_mem_dbg.rs
@@ -13,7 +13,7 @@ use core::{marker::PhantomData, sync::atomic::*};
 use std::collections::{HashMap, HashSet};
 
 use crate::impl_mem_size::MemSizeHelper2;
-use crate::{impl_mem_size::MemSizeHelper, CopyType, DbgFlags, MemDbgImpl};
+use crate::{CopyType, DbgFlags, MemDbgImpl, impl_mem_size::MemSizeHelper};
 
 /// Implements [`MemDbg`] using the default implementation of [`MemDbgImpl`].
 macro_rules! impl_mem_dbg {
