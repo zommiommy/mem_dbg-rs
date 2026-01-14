e470c86112b7ae885a28f5a1bd02fe780a9b1dbd
Luca <cappelletti.luca94@gmail.com>
2026-01-14 22:10:39 +0100

    Updated tests

--- mem_dbg/tests/test_btree.rs
+++ mem_dbg/tests/test_btree.rs
@@ -10,13 +10,10 @@ fn test_btree_map() {
 
     // Size should be:
     // size_of::<BTreeMap>() +
-    // 2 * size_of::<u8>() (keys) +
-    // 2 * size_of::<u8>() (values)
-    // = size_of::<BTreeMap>() + 4 bytes.
-    // This implementation ignores internal node overhead.
+    // Node size (38 bytes for 2 elements in a leaf with B=6, 16b header, aligned)
 
     let size = map.mem_size(SizeFlags::default());
-    let expected = std::mem::size_of::<BTreeMap<u8, u8>>() + 4;
+    let expected = std::mem::size_of::<BTreeMap<u8, u8>>() + 38;
     assert_eq!(size, expected);
 }
 
@@ -27,7 +24,7 @@ fn test_btree_set() {
     set.insert(2u8);
 
     let size = set.mem_size(SizeFlags::default());
-    let expected = std::mem::size_of::<BTreeSet<u8>>() + 2;
+    let expected = std::mem::size_of::<BTreeSet<u8>>() + 27;
     assert_eq!(size, expected);
 }
 
@@ -37,11 +34,10 @@ fn test_btree_map_recursive() {
     map.insert(1u8, vec![1u8, 2u8]);
 
     // size_of::<BTreeMap>
-    // + 1 * u8
-    // + 1 * Vec (size_of::<Vec> + 2 * u8)
+    // + Leaf Node size (296 bytes)
+    // + Vec heap overhead (2 bytes)
 
     let size = map.mem_size(SizeFlags::default());
-    let expected =
-        std::mem::size_of::<BTreeMap<u8, Vec<u8>>>() + 1 + std::mem::size_of::<Vec<u8>>() + 2;
+    let expected = std::mem::size_of::<BTreeMap<u8, Vec<u8>>>() + 298;
     assert_eq!(size, expected);
 }
