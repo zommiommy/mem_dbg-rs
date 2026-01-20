d20e6ca8545598654e4d55ea311b9dc5e1925921
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2026-01-20 18:26:07 -0300

    mem_dbg is now separately tested for UnsafeCell/Cell)

--- mem_dbg/tests/test_unsafe_cell.rs
+++ mem_dbg/tests/test_unsafe_cell.rs
@@ -3,6 +3,7 @@
 
 use core::cell::UnsafeCell;
 use mem_dbg::*;
+use std::cell::Cell;
 
 #[test]
 fn test_unsafe_cell_in_struct() {
@@ -15,5 +16,25 @@ fn test_unsafe_cell_in_struct() {
         field: UnsafeCell::new(42),
     };
     let size = s.mem_size(SizeFlags::default());
-    assert_eq!(size, 4)
+    assert_eq!(size, 4);
+
+    // This is just to let miri test the unsafe block
+    let _ = s.mem_dbg(DbgFlags::default());
+}
+
+#[test]
+fn test_cell_in_struct() {
+    #[derive(MemSize, MemDbg)]
+    struct Test {
+        field: Cell<i32>,
+    }
+
+    let s = Test {
+        field: Cell::new(42),
+    };
+    let size = s.mem_size(SizeFlags::default());
+    assert_eq!(size, 4);
+
+    // This is just to let miri test the unsafe block
+    let _ = s.mem_dbg(DbgFlags::default());
 }
